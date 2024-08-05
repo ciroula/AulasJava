1e618b100f4a91757e00e3522312e2ad47f7f087
m1ch3lld <159503125+m1ch3lld@users.noreply.github.com>
2024-08-05 14:27:41 +0000

    gsdfhn

---
+++ aula_2/src/main/java/com/example/OperadoresLogicos.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        //Declaracao de variavel
+        boolean temSol = true;
+        boolean folga = false;
+
+
+        //so ira passar se as duas condicoes forem verdadeiras
+        if (temSol && folga){
+            System.out.println("Irei passear hoje! =D");
+        }else{
+            System.out.println("Nao irei passear! =C");
+        }
+
+    }
+}
